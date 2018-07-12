c4573764b23b456b35f1fca63ce652e28c915391
homirun <hatunemiku.moco@gmail.com>
2018-07-12 18:15:12 +0900

    レイアウトをTabPanelに変更。 メモリリークしてたのでそれを直した。 JavaDoc書いた

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,5 @@
 package sample;
 
-import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import javax.xml.soap.Text;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,26 +22,22 @@ public class Controller implements Initializable {
     private TextArea headerArea;
 
 
-    @FXML
-    public void load() {
-
-    }
-
     // submit
     @FXML
-    public void onButtonClick(ActionEvent evt) {
+    public void onSubmitClick(ActionEvent evt) {
         //TODO: 例外処理
         try {
             HttpConnection httpConnection = new HttpConnection(urlField.getText());
             headerArea.setText(httpConnection.sendRequest());
             System.out.println("submit");
         }catch (IOException e){
+            // TODO: GUIにエラーを表示させる
             System.out.println(e);
         }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        urlLabel.setText("hoge");
+
     }
 }
--- src/sample/HttpConnection.java
+++ src/sample/HttpConnection.java
@@ -6,19 +6,33 @@ import okhttp3.Response;
 
 import java.io.IOException;
 
+/**
+ * HTTPでの接続に関するクラス
+ */
 public class HttpConnection {
     private OkHttpClient client = new OkHttpClient();
     private String url;
 
+    /**
+     * コンストラクタ
+     * @param url リクエストを送りたいurl
+     */
     public HttpConnection(String url) {
         this.url = url;
     }
 
     //TODO: メソッドはenumで
+    /**
+     * Requestを送る
+     * @return ResponseのHeader
+     * @throws IOException
+     */
     public String sendRequest() throws IOException {
         Request req = new Request.Builder().url(url).build();
 
         Response res = client.newCall(req).execute();
-        return res.headers().toString();
+        String resHeader = res.headers().toString();
+        res.close();
+        return resHeader;
     }
 }
