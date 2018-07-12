4b7f834a6a51b6aae3c3949be3ad380686751476
homirun <hatunemiku.moco@gmail.com>
2018-07-12 20:18:06 +0900

    不正なurlを投げられた場合にGUIでエラー表示。 optionモーダルウインドウを実装

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,11 +2,17 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -25,15 +31,33 @@ public class Controller implements Initializable {
     // submit
     @FXML
     public void onSubmitClick(ActionEvent evt) {
-        //TODO: 例外処理
         try {
             HttpConnection httpConnection = new HttpConnection(urlField.getText());
             headerArea.setText(httpConnection.sendRequest());
+
+            urlField.setStyle("");
+
             System.out.println("submit");
         }catch (IOException e){
             // TODO: GUIにエラーを表示させる
+
             System.out.println(e);
+        }catch(IllegalArgumentException e){  // urlが不正
+            urlField.setStyle("-fx-base: #FF0000");
         }
+    }
+
+    // option
+    @FXML
+    public void onOptionClick(ActionEvent evt) throws IOException{
+        // optionStageを宣言
+        Stage optionStage = new Stage();
+        // option.fxmlをロードしたWindowを作成
+        Parent root = FXMLLoader.load(getClass().getResource("option.fxml"));
+        optionStage.initModality(Modality.WINDOW_MODAL);
+        optionStage.initOwner(((Node)evt.getSource()).getScene().getWindow());
+        optionStage.setScene(new Scene(root));
+        optionStage.show();
     }
 
     @Override
